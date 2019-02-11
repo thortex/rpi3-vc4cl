635c0c4e25cd2d9c3402215a03a0c3e2bb6e5e5f
Thor Watanabe <thormac@gmail.com>
2019-02-11 16:34:28 +0900

    Modified VC4C/VC4CL binary package installation scripts.

--- 022_install_vc4c.sh
+++ 022_install_vc4c.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 R=v0.4-3
-D=20190206
+D=0.4-20190206
 U=https://github.com/thortex/rpi3-vc4cl/releases/download/
-F=vc4c_${R}-${D}_armhf.deb
+F=vc4c_${D}_armhf.deb
 wget -c $U$R/$F
 sudo dpkg -i $F
--- 032_install_vc4cl.sh
+++ 032_install_vc4cl.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -x
-R=v0.4-20190207
+D=0.4-20190207
+R=v0.4-3
 U=https://github.com/thortex/rpi3-vc4cl/releases/download/
-F=vc4cl_${R}-1_armhf.deb
+F=vc4cl_${D}_armhf.deb
 wget -c $U$R/$F
 sudo dpkg -i $F
